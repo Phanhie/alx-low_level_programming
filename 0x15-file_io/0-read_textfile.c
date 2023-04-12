97eace829fb5e73c4e8e4585b92c5162e4737195
Phanhie <yangestephanied@gmail.com>
2023-04-12 14:49:24 -0700

    File Input & Output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * function that reads a text file and prints it to the POSIX standard output.
+ * @filename: filename.
+ * @letters: number of letters it should read and print
+ *
+ * Return: numbers of letters printed. if it fails, return 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
+}
